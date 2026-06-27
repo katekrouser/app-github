f2d58cd6f11778e11048b911ecc2b04662f2db4a
Kate Krovyakova <katekrotestone@gmail.com>
2026-06-27 17:45:54 +0300

    created tests for adding user in table and filling inline form

---
+++ tests/page-objects.ts/pageManager.ts
@@ -0,0 +1,30 @@
+import { Page } from "@playwright/test";
+import { SmartTablePage } from "./smartTablePage";
+import { FormLayoutsPage } from "./formLayoutsPage";
+import { NavigationPage } from "./navigationPage"
+
+export class PageManager {
+    private readonly page: Page
+    private readonly smartTablePage: SmartTablePage
+    private readonly formLayoutsPage: FormLayoutsPage
+    private readonly navigationPage: NavigationPage
+
+    constructor(page: Page) {
+        this.page = page
+        this.smartTablePage = new SmartTablePage(this.page)
+        this.formLayoutsPage = new FormLayoutsPage(this.page)
+        this.navigationPage = new NavigationPage(this.page)
+    }
+
+    navigateTo() {
+        return this.navigationPage
+    }
+
+    onSmartTablesPage() {
+        return this.smartTablePage
+    }
+
+    onFormLayoutsPage() {
+        return this.formLayoutsPage
+    }
+}
