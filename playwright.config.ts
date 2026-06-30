e2000a5b43a141b12e3838e352086535ca9c4cc8
Kate Krovyakova <katekrotestone@gmail.com>
2026-06-30 19:30:54 +0300

    configuration updates for job

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'npm start', 
+    url: 'http://localhost:4200/',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000, 
+  },
   testDir: './tests',
 
   fullyParallel: true,
