d31545b0f02d7400557943359c717ebd744c1601
Kate Krovyakova <katekrotestone@gmail.com>
2026-07-02 12:52:00 +0300

    removed helperBase class and updated basePage

--- tests/page-objects/basePage.ts
+++ tests/page-objects/basePage.ts
@@ -1,12 +1,29 @@
-import { Page } from "@playwright/test";
-import { HelperBase } from "./helperBase";
+import { expect, Page, Locator } from "@playwright/test";
 
-export class BasePage extends HelperBase {
+export class BasePage {
+    protected readonly page: Page
+
     constructor(page: Page) {
-        super(page)
+        this.page = page
     }
 
      async reload() {
         await this.page.reload();
      }
+
+     async waitForSpinnerToDisappear() {
+             await expect(this.page.locator('.spinner')).toBeHidden();
+         }
+     
+         async scrollIntoView(locator: Locator) {
+             await locator.scrollIntoViewIfNeeded();
+         }
+     
+         async waitUntilVisible(locator: Locator) {
+             await expect(locator).toBeVisible();
+         }
+     
+         async waitUntilHidden(locator: Locator) {
+             await expect(locator).toBeHidden();
+         }
 }
